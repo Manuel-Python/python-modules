92fbebe1baa3cc5f4b650eb53795c59eca2ff876
Manuel Carvalho <burrowsrd@gmail.com>
2020-12-02 07:27:32 +1100

    Added turtledemo.py

---
+++ turtledemo.py
@@ -0,0 +1,16 @@
+# import turtle
+from turtle import * #no need for turtle object not recomended using *
+
+
+done = "Well done, you have finished your drawing"
+
+# noinspection PyUnresolvedReferences
+forward(150)
+right(250)
+forward(150)
+circle(75)
+
+#turtle.forward(150)
+
+done()
+print(done)
