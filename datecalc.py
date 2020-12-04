65c52217f6726d8b44908268ff552ccc7b0a7a5b
Manuel Carvalho <burrowsrd@gmail.com>
2020-12-05 08:49:31 +1100

    Added datecalc.py (2)

--- datecalc.py
+++ datecalc.py
@@ -2,13 +2,15 @@ import time
 
 print(time.gmtime(0))
 
+print(time.time())
+
 time_here = time.localtime()
 print(time_here)
 print("Year:", time_here[0], time_here.tm_year)
 print("Month:", time_here[1], time_here.tm_mon)
 print("Day:", time_here[2], time_here.tm_mday)
 
-import time
+#import time
 from time import time as my_timer
 import random
 
